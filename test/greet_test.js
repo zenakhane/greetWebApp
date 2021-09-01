b0bf4ae45bd934ec4887ff2359773ae7506ba7dc
Zenakhane <lilliantyiso@gmail.com>
2021-09-01 11:08:32 +0200

    unit test

--- test/greet_test.js
+++ test/greet_test.js
@@ -1,54 +1,59 @@
-module.exports = describe('Greet', function () {
-    it('Should show name greeted in venda', function () {
-        var message = Greetings()
+let assert = require("assert");
+let Greetings1 = require("../greet");
+
+
+
+describe('Greet', function () {
+    it('Should show name greeted in IsiXhosa', function () {
+        var message = Greetings1()
         message.setNames('Zena')
-        assert.equal(message.greetMessage("Zena", "Venda"), 'Ndaa, Zena',);
+        assert.equal(message.greetMessage("Zena", "IsiXhosa"), 'Molo, Zena',);
     });
 
-    it('Should show name greeted in Nama', function () {
-        var message = Greetings()
-        message.setNames('Zena')
-        assert.equal(message.greetMessage("Asive", "Nama"), 'Halau, Asive');
+    it('Should show name greeted in English', function () {
+        var message = Greetings1()
+        message.setNames('Asive')
+        assert.equal(message.greetMessage("Asive", "English"), 'Hello, Asive');
 
 
     });
 
-    it('Should show name greeted in Tsonga', function () {
-        var message = Greetings()
+    it('Should show name greeted in IsiZulu', function () {
+        var message = Greetings1()
         message.setNames('Lakhe')
-        assert.equal(message.greetMessage("Lakhe", "Tsonga"), 'Ahee, Lakhe');
+        assert.equal(message.greetMessage("Lakhe", "IsiZulu"), 'Sawubona, Lakhe');
 
     });
 });
 describe('Set names', function () {
     it('Greet a name that has not been greeted', function () {
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Bob')
-        assert.equal(message.greetMessage("Bob", "Venda"), 'Ndaa, Bob',);
+        assert.equal(message.greetMessage("Bob", "IsiXhosa"), 'Molo, Bob',);
     });
     it('Greet a name that has not been greeted', function () {
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Naomi')
-        assert.equal(message.greetMessage("Naomi", "Tsonga"), 'Ahee, Naomi',);
+        assert.equal(message.greetMessage("Naomi", "English"), 'Hello, Naomi',);
 
     });
 
 });
 describe('Counter', function () {
     it('Should count how many names have been greeted', function () {
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Zena')
         assert.equal(1, message.setNameCount())
     })
     it('Should count how many names have been greeted', function () {
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Zena')
         message.setNames('Lakhe')
         message.setNames('Naomi')
         assert.equal(3, message.setNameCount())
     })
     it('Should count how many names have been greeted', function () {
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Bob')
         message.setNames('Zena')
         message.setNames('Lakhe')
@@ -56,7 +61,7 @@ describe('Counter', function () {
         assert.equal(4, message.setNameCount())
     })
     it('Should not count a name twice', function(){
-        var message = Greetings()
+        var message = Greetings1()
         message.setNames('Zena')
         message.setNames('Zena')
         message.setNames('Lakhe')
